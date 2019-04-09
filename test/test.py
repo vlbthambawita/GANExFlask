d3d1ae408420a70af3d525604399c706acc674e5
vajira <vlbthambawita@gmail. com>
2019-04-09 21:55:31 +0200

    before merge with rog

--- test/test.py
+++ test/test.py
@@ -1,10 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Test data
+post_data = [
+    {
+        'author': 'Vajira',
+        'title': 'Test title 1',
+        'content': 'HElo',
+        'date_posted': '02-04-2018'
+    
+    },
+    {
+        'author': 'Lasantha',
+        'title': 'Test title 2',
+        'content': 'HElo world',
+        'date_posted': '06-03-2011'
+    
+    }
+]
+
 @app.route("/")
-def hello():
-    return "Hello"
+@app.route("/home")
+def home():
+    return render_template("home.html", posts= post_data)
+
+@app.route("/about")
+def about():
+    return "This is about page"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
