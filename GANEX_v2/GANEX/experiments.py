22f93f0908e7965d1ee7b683c91a33dff3d741df
vajira <vlbthambawita@gmail. com>
2019-07-30 13:15:23 +0200

    delete experiments

--- GANEX_v2/GANEX/experiments.py
+++ GANEX_v2/GANEX/experiments.py
@@ -25,7 +25,10 @@ def create(pid):
 
     #
     col_gans = (db["gantypes"].find({},{"_id":0}))
-    #col = db["experiment"] # projects table
+    col_exp = db["experiments"] # experiments table
+
+    col_pro_exp = col_exp.find({"pid":pid})
+
     gan_types = []
 
     for g in col_gans:
@@ -47,14 +50,32 @@ def create(pid):
     #for p in all_projects:
      #   print(p)
 
-    #if pro_form.validate_on_submit():
+    if exp_form.validate_on_submit():
 
-        #if error is None:
-            #pdict = {"name":pro_form.projectName.data,"path":pro_form.projectPath.data}
+        if error is None:
+            exp_dict = {"name":exp_form.expName.data, "type":exp_form.ganType.data, "pid": pid}
 
-            #x = col.insert_one(pdict)
-            #print(x.inserted_id) #out.inserted_id
+            x = col_exp.insert_one(exp_dict)
+            print(x.inserted_id) #out.inserted_id
             # flash(x.inserted_id) # remove this one, if redirect the page
             #return redirect(url_for('experiments.create'))
 
-    return render_template('experiments/create.html', form=exp_form, pid=pid)
+    return render_template('experiments/create.html', form=exp_form, pid=pid, exps=col_pro_exp)
+
+@bp.route('/<pid>/<expid>/deleteExp', methods=('GET',))
+def deleteExp(pid, expid):
+    db = get_db()
+    exp_col = db['experiments']
+    query = {"_id":ObjectId(expid)} # need this Object ID
+    
+    
+
+    x =exp_col.delete_one(query) # delete given expid
+
+    
+    print(query)
+    #pid = request.id
+    print(expid)
+    print(x.deleted_count)
+
+    return redirect(url_for('experiments.create', pid=pid))
